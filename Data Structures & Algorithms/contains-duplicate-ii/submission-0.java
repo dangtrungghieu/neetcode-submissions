a989b51f9a723d7dcf5f9e43d7c27c2304369224
Đặng Trung Hiếu <147295951+dangtrungghieu@users.noreply.github.com>
2026-09-21 21:49:22 +0700

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(nums[0],0);
+        for(int i = 1;i < nums.length;i++){
+            if( map.containsKey(nums[i]) 
+                && Math.abs(map.get(nums[i]) - i) <= k){
+                    return true;
+                }else{
+                    map.put(nums[i],i);
+                }
+        }
+        return false;
+    }
+}
